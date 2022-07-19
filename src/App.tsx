b6a87b595deca32653046a6d10ceec3b2e92c50f
AntonKrasouski <antohkad21@gmail.com>
2022-07-19 22:00:15 +0300

    Add TodoList.tsx component with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import './App.css';
+import {TodoList} from "./TodoList";
+import {TaskType} from "./TodoList";
+
+//GRUD - create read update delete
+//GUI - graphic user interface
 
 function App() {
+    const tasks_1: Array<TaskType> = [
+        {id: 1, title: "HTML", isDone: true},
+        {id: 2, title: "CSS", isDone: true},
+        {id: 3, title: "JS/TS", isDone: false}
+    ]
+    const tasks_2: Array<TaskType> = [
+        {id: 1, title: "React", isDone: true},
+        {id: 2, title: "Angular", isDone: false},
+        {id: 3, title: "Vue", isDone: false}
+    ]
+    const tasks_3: Array<TaskType> = [
+        {id: 1, title: "PHP", isDone: false},
+        {id: 2, title: "Python", isDone: false},
+        {id: 3, title: "Go", isDone: false}
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList tasks = {tasks_1} title= "What to learn"/>
+            <TodoList tasks = {tasks_2} title= "What to study"/>
+            <TodoList tasks = {tasks_3} title= "What to read"/>
         </div>
     );
 }
